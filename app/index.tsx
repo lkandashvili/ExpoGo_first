5d93e9e5a729289005ecac0a3e67e7938c65bcb2
lkandashvili <lkandashvili77@gmail.com>
2025-09-25 12:34:08 +0400

    fc

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { Text, View, Button, TextInput} from "react-native";
+import { useState } from "react";
+
+export default function Index() {
+
+  const [value, setValue] = useState("")
+  const [buttonText, setButtonText] = useState("")
+
+  const handleButton =() => {
+    setButtonText(value)
+  }
+
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Text>{buttonText}</Text>
+      <Button onPress={handleButton} title="Damachiiree"></Button>
+      <TextInput onChangeText={(e) => setValue(e)} style={{
+        width:200,
+        height:60,
+
+        backgroundColor:"grey",
+        borderWidth:1
+        }}/>
+    </View>
+  );
+}
